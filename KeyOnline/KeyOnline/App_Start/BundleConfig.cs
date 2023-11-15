f7f1731b0bac246fe515b240f5a7bf808fff9b68
VTI\duc.phamvan <kimconoc@gmail.com>
2023-11-15 18:55:16 +0700

    push code

---
+++ KeyOnline/KeyOnline/App_Start/BundleConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Seafood
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/js").Include());
+            bundles.Add(new StyleBundle("~/bundles/css").Include());
+        }
+    }
+}
---
+++ KeyOnline/KeyOnline/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Web.Http.Results;
+using System.Web.Mvc;
+
+namespace Seafood.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult KeyOnline()
+        {
+            return View();
+        }
+    }
+}
