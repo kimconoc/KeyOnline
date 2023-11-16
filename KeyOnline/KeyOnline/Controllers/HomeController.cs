31a0e5dfbd90adfc0f9b676c8cb5e75d7f128f02
VTI\duc.phamvan <kimconoc@gmail.com>
2023-11-16 11:49:22 +0700

    push code

--- KeyOnline/KeyOnline/Controllers/HomeController.cs
+++ KeyOnline/KeyOnline/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using Enyim.Caching.Memcached;
+using Seafood.Models;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Net;
 using System.Web.Http.Results;
 using System.Web.Mvc;
 
@@ -9,6 +13,63 @@ namespace Seafood.Controllers
         public ActionResult KeyOnline()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult ExecuteKeyOnline(int valueChonmang, int valueCard, string valueTxtuser , string valueTxtpin, string valueTxtseri)
+        {
+            return Json(Success_Request(true));
+        }
+
+        protected DataResponse<TRequest> Success_Request<TRequest>(TRequest data)
+        {
+            return new DataResponse<TRequest>()
+            {
+                Data = data,
+                Success = true,
+                StatusCode = (int)HttpStatusCode.OK,
+                Message = "Thành công"
+            };
+        }
+
+        protected StatusResponse Success_Request()
+        {
+            return new StatusResponse()
+            {
+                Success = true,
+                StatusCode = (int)HttpStatusCode.OK,
+                Message = "Thành công"
+            };
+        }
+
+        protected StatusResponse Not_Found(string message = "")
+        {
+            return new StatusResponse()
+            {
+                Success = true,
+                StatusCode = (int)HttpStatusCode.NotFound,
+                Message = string.IsNullOrEmpty(message) ? "Không tìm thấy dữ liệu" : message
+            };
+        }
+
+        protected StatusResponse Bad_Request(string message = "")
+        {
+            return new StatusResponse()
+            {
+                Success = false,
+                StatusCode = (int)HttpStatusCode.BadRequest,
+                Message = string.IsNullOrEmpty(message) ? "Dữ liệu định dạng sai" : message
+            };
+        }
+
+        protected StatusResponse Server_Error(string message = "")
+        {
+            return new StatusResponse()
+            {
+                Success = false,
+                StatusCode = (int)HttpStatusCode.InternalServerError,
+                Message = string.IsNullOrEmpty(message) ? "Có lỗi trong quá trình xử lý" : message
+            };
         }
     }
 }
